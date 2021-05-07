b49072ece48be2cf961b6102fa5853f892239ac8
Murali03M <murali732000@gmail.com>
2021-05-07 17:31:33 +0530

    Java End-of-file

---
+++ Day_3/Hackerrank/Solution1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public  class Solution1 
+ {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int i=1;
+        while(sc.hasNext())
+        {
+        String str=sc.nextLine();
+        System.out.println(i+" "+str);
+        i++;
+    }
+    }
+}
