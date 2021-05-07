4b4b124b85ca442b1e129790465f19216f37cbec
Murali03M <murali732000@gmail.com>
2021-05-07 14:09:28 +0530

    Default constructor

---
+++ Day_3/Constructor/Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+           private int emp_id;
+           private String emp_name;
+
+            public Employee()
+            {
+                System.out.println("Default constructor");
+            }
+
+           public static void main(String[] args)
+           {
+               Employee e=new Employee();
+           }
+}
