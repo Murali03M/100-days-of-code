b2a01c56431150f1d07c5b35a7bfc0e65abe4272
Murali03M <murali732000@gmail.com>
2021-05-08 21:59:23 +0530

    Day 3 completed

---
+++ Day_4/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    static void mymethod()
+    {
+        System.out.println("I just got excuted");
+       }
+
+       public static void main(String[] args)
+       {
+         mymethod();
+         mymethod();
+         mymethod();
+       }
+}
---
+++ Day_4/Main1.java
@@ -0,0 +1,14 @@
+public class Main1 {
+    
+    static void mymethod(String name)
+    {
+        System.out.println("Output:"+name);
+       }
+
+       public static void main(String[] args)
+       {
+         mymethod("Jeni");
+         mymethod("Annamalai");
+         mymethod("Kiruba");
+       }
+}
