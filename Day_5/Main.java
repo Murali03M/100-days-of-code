f6a4dd26f11eb68d15f03530fc372ad17a9d5ae5
Murali03M <murali732000@gmail.com>
2021-05-09 23:11:30 +0530

    Day_5 completed.

---
+++ Day_5/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args) {
+        {
+            int[] a={1,2,3,4,5,6};
+            for(int i=0;i<5;i++)
+            {
+                System.out.println("output :"+a[i]);
+            }
+
+        }
+    }
+}
