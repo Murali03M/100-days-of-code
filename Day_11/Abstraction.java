74cb23c2da34abe87d82dc553182ea33f01c5670
Murali03M <murali732000@gmail.com>
2021-05-16 22:58:47 +0530

    completed

--- Day_11/Abstraction.java
+++ Day_11/Abstraction.java
@@ -1,4 +1,3 @@
-
 public class abstraction {
 	public static void main(String[] args)
 	{
---
+++ Day_12/ListDemo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ListDemo
+{
+    public static void main(String args[])
+    {
+        List<Integer> l1=new ArrayList<Integer>();
+        l1.add(0,1);
+        l1.add(1,2);
+        l1.add(2,3);
+        System.out.println(l1);
+        List<Integer>  l2=new ArrayList<Integer>();
+        l2.add(1);
+        l2.add(2);
+        l2.add(3);
+        System.out.println(l2);
+
+
+
+    }
+}  
