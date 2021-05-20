0a7f2aec8f6cb59dfa384592041b219b0ddee1bc
Murali03M <murali732000@gmail.com>
2021-05-20 22:33:38 +0530

    HashSet

---
+++ Day_14/Main1.java
@@ -0,0 +1,14 @@
+import java
+import java.util.HashSet; util HashSet;
+
+public class Main1
+{
+    public static void main(String args[])
+    {
+        HashSet<String> cars=new HashSet<String>();
+        cars.add(benz);
+        cars.add(marudi);
+        cars.add(suzuki);
+        system.out.println(cars);
+        }
+}
