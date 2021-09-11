dbb4d5a46ee191249b536b4203310ee4244fff68
Murali03M <murali732000@gmail.com>
2021-09-11 22:18:28 +0530

    java Array

---
+++ Day_29/src/com/day29/Array.java
@@ -0,0 +1,19 @@
+package com.day29;
+
+public class Array {
+
+	  public static void main(String args[])
+	  {
+		int[] arr=new int[5];
+		  arr[0]=1;
+		  arr[1]=2;
+		  arr[3]=4;
+		  arr[4]=5;
+		  arr[2]=7;
+		  for(int i=0;i<5;i++)
+		  {
+			 System.out.println(arr[i]);
+		  }
+		  
+	  }
+}
