a6cce224bc4f9493e42f9d0a6af14ae8e2d3a2d9
Murali03M <murali732000@gmail.com>
2021-09-30 22:55:42 +0530

    code

--- Day_36/src/com/accenture/stack/Testert.java
+++ Day_36/src/com/accenture/stack/Testert.java
@@ -1,19 +1,15 @@
 package com.accenture.stack;
 
 public class Testert {
+	int x=10;
+	static {
+		x=10;
+		System.out.println(x);
+	}
 public static void main(String[] args) {
-int sum = 0;
-String test="hello i love java";
-String str=test.indexOf('e')+" "+test.indexOf('a')+" "+test.indexOf(' ')+" "+test.indexOf('a');
-System.out.println(str);
-for(int
-i=0;i<str.length();i++){
-if(str.charAt(i)!=' ') {
-	System.out.println(str.charAt(i));
-sum = sum+str.charAt(i);
-}
-}
-System.out.print(sum);
+  
+	Testert d=new Testert();
+	System.out.println(d.x);
 }
 }
 
