28def155991a9140862412176bbb6ac31bf6969e
Murali03M <murali732000@gmail.com>
2021-10-16 22:05:37 +0530

    combo box code

---
+++ Day_45/src/com/swing/Combo.java
@@ -0,0 +1,25 @@
+package com.swing;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+
+public class Combo {
+
+	  JFrame f;
+	  Combo()
+	  {
+		  f=new JFrame("combo box");
+		  String[] lang= {"c program","java","python"};
+          JComboBox b=new JComboBox(lang);
+          b.setBounds(50, 50, 100, 30);
+          f.add(b);
+          f.setLayout(null);
+          f.setSize(400,400);
+          f.setVisible(true);
+	  }
+	  public static void main(String args[])
+	  {
+		  new Combo();
+		  
+	  }
+}
